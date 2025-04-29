103b87a72a61374b8f9c3605e2380ed2306e77e5
hokhgol <tdh2000@bk.ru>
2025-04-29 20:36:50 +0500

    v1.1

--- ping-pong.py
+++ ping-pong.py
@@ -1,10 +1,16 @@
 from pygame import *
+font.init()
 
 win_width = 700 
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption('ping_pong')
 window.fill((100, 200, 200))
+speed_x = 3
+speed_y = 3
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 class GameSprite(sprite.Sprite):
     def __init__ (self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -20,19 +26,20 @@ class GameSprite(sprite.Sprite):
 class Player (GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.Y > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.Y < win_height - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_W] and self.rect.Y > 5:
-            self.rect.x -= self.speed
-        if keys[K_S] and self.rect.Y < win_height - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-rocket_r = Player('rocket.jpg', 5, win_height - 100, 80, 100, 10)
-
+rocket_l = Player('rocket3.png', 5, win_height - 100, 20, 100, 10)
+rocket_r = Player('rocket3.png', 670, win_height - 100, 20, 100, 10)
+ball = GameSprite('ball3.png', 0, win_height - 500, 75, 75, 10)
 
 game =True
 finish = False
@@ -45,10 +52,27 @@ while game :
             game = False
     
     if finish != True:
+        window.fill((100, 200, 200))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 425 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(rocket_l, ball) or sprite.collide_rect(rocket_r, ball):
+            speed_x *= -1
 
-        rocket_r.update_r()
+        rocket_l.update_l()
+        rocket_r.update_r()      
+        rocket_l.reset()
         rocket_r.reset()
+        ball.reset()
 
+        if ball.rect.x < 0 :
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > 700 :
+            finish = True
+            window.blit(lose2, (200, 200))        
 
         display.update()
 
