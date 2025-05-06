44c46d3c48be407f2dfbc68c779654694bd36598
hokhgol <tdh2000@bk.ru>
2025-05-06 20:35:27 +0500

    v1.2

--- ping-pong.py
+++ ping-pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+from time import sleep
 font.init()
 
 win_width = 700 
@@ -11,6 +12,10 @@ speed_y = 3
 font1 = font.Font(None, 35)
 lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+wins_r = 0
+wins_l = 0
+
+
 
 class GameSprite(sprite.Sprite):
     def __init__ (self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -37,9 +42,9 @@ class Player (GameSprite):
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-rocket_l = Player('rocket3.png', 5, win_height - 100, 20, 100, 10)
-rocket_r = Player('rocket3.png', 670, win_height - 100, 20, 100, 10)
-ball = GameSprite('ball3.png', 0, win_height - 500, 75, 75, 10)
+rocket_l = Player('rocket3.png', 5, win_height - 250, 20, 100, 10)
+rocket_r = Player('rocket3.png', 675, win_height - 250, 20, 100, 10)
+ball = GameSprite('ball3.png', 350, win_height - 250, 75, 75, 10)
 
 game =True
 finish = False
@@ -60,20 +65,47 @@ while game :
         if sprite.collide_rect(rocket_l, ball) or sprite.collide_rect(rocket_r, ball):
             speed_x *= -1
 
+        player_l = font1.render(str(wins_l),  True, (255, 255, 255))
+        player_r = font1.render(str(wins_r), True, (255, 255, 255))
         rocket_l.update_l()
         rocket_r.update_r()      
         rocket_l.reset()
         rocket_r.reset()
         ball.reset()
+        window.blit(player_l, (10, 10))
+        window.blit(player_r, (650, 10))
 
         if ball.rect.x < 0 :
             finish = True
             window.blit(lose1, (200, 200))
+            wins_r += 1
 
         if ball.rect.x > 700 :
             finish = True
-            window.blit(lose2, (200, 200))        
+            window.blit(lose2, (200, 200))
+            wins_l += 1 
 
         display.update()
+    else :
+        if ((wins_l % 5) == 0 or (wins_r % 5) == 0) and ((wins_l > 0) or (wins_l > 0)):
+            if speed_x > 0:
+                speed_x += 1
+            else:
+                speed_x -= 1
+
+            if speed_y > 0:
+                speed_y += 1
+            else:
+                speed_y -= 1
+
+
+        sleep(2)
+        ball.rect.x = win_width/2
+        ball.rect.y = win_height/2
+        rocket_l.rect.x = 5
+        rocket_l.rect.y = win_height -250
+        rocket_r.rect.x = 675
+        rocket_r.rect.y = win_height - 250  
+        finish = False
 
     clock.tick(FPS)
